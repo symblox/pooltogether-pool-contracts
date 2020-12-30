8ed50bca1180bcd80612c8fa2ab221a6ed6d4ab3
Symblox Dev <symblox@protonmail.com>
2020-12-31 00:42:47 +0800

    Removed SVLX and WVLX from this project

--- contracts/SVLX/interfaces/IValidatorSetAuRa.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.6.12;
-
-interface IValidatorSetAuRa {
-    function miningByStakingAddress(address) external view returns (address);
-
-    function areDelegatorsBanned(address) external view returns (bool);
-
-    function isValidatorOrPending(address) external view returns (bool);
-}
